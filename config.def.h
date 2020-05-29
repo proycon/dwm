1f3aaa07a83121e1c01bfc6b7ae315700aa347b2
Maarten van Gompel <proycon@anaproy.nl>
2020-05-29 22:37:10 +0200

    applied fullgaps patch

--- config.def.h
+++ config.def.h
@@ -2,6 +2,7 @@
 
 /* appearance */
 static const unsigned int borderpx  = 3;        /* border pixel of windows */
+static const unsigned int gappx     = 5;        /* gaps between windows */
 static const unsigned int snap      = 32;       /* snap pixel */
 static const unsigned int systraypinning = 0;   /* 0: sloppy systray follows selected monitor, >0: pin systray to monitor X */
 static const unsigned int systrayspacing = 2;   /* systray spacing */
@@ -137,10 +138,9 @@ static Key keys[] = {
 	{ MODKEY|ShiftMask,             XK_apostrophe,  togglefloating, {0} },
 	{ MODKEY,                       XK_backslash,  togglefloating, {0} },
 	{ MODKEY,                       XK_s,      togglesticky,   {0} },
-	{ MODKEY,                       XK_comma,  focusmon,       {.i = -1 } },
-	{ MODKEY,                       XK_period, focusmon,       {.i = +1 } },
-	{ MODKEY|ShiftMask,             XK_comma,  tagmon,         {.i = -1 } },
-	{ MODKEY|ShiftMask,             XK_period, tagmon,         {.i = +1 } },
+	{ MODKEY,                       XK_comma,  setgaps,        {.i = -3 } },
+	{ MODKEY,                       XK_period,  setgaps,        {.i = +3 } },
+	{ MODKEY|ShiftMask,             XK_period,  setgaps,        {.i = 0  } },
 	TAGKEYS(                        XK_1,                      0)
 	TAGKEYS(                        XK_2,                      1)
 	TAGKEYS(                        XK_3,                      2)
