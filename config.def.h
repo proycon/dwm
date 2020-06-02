30ed8ad65a7eb5db5060add21f6ee46df0a19bb9
Maarten van Gompel <proycon@anaproy.nl>
2020-06-02 18:31:38 +0200

    adding alias keys for monitor switching

--- config.def.h
+++ config.def.h
@@ -114,6 +114,10 @@ static Key keys[] = {
 	{ MODKEY,                       XK_period, focusmon,       {.i = +1 } },
 	{ MODKEY|ShiftMask,             XK_comma,  tagmon,         {.i = -1 } },
 	{ MODKEY|ShiftMask,             XK_period, tagmon,         {.i = +1 } },
+	{ MODKEY,                       XK_minus,  focusmon,       {.i = -1 } },
+	{ MODKEY,                       XK_equal, focusmon,       {.i = +1 } },
+	{ MODKEY|ShiftMask,             XK_minus,  tagmon,       {.i = -1 } },
+	{ MODKEY|ShiftMask,             XK_equal, tagmon,       {.i = +1 } },
 	TAGKEYS(                        XK_1,                      0)
 	TAGKEYS(                        XK_2,                      1)
 	TAGKEYS(                        XK_3,                      2)
