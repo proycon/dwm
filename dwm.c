1927ac7a3e599d31fbbf0dd4c995edcf236ab775
Maarten van Gompel <proycon@anaproy.nl>
2020-06-09 00:19:00 +0200

    declarations

--- dwm.c
+++ dwm.c
@@ -245,6 +245,8 @@ static int isdescprocess(pid_t p, pid_t c);
 static Client *swallowingclient(Window w);
 static Client *termforwin(const Client *c);
 static pid_t winpid(Window w);
+static void swallow(Client *p, Client *c);
+static void unswallow(Client *c);
 
 /* variables */
 static const char broken[] = "broken";
