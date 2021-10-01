1160ef48c3915910342ddb0d9b6cbf0c59603639
Maarten van Gompel <proycon@anaproy.nl>
2021-10-01 17:53:29 +0200

    only draw status on selected monitor

--- dwm.c
+++ dwm.c
@@ -900,7 +900,7 @@ drawbar(Monitor *m)
 		stw = getsystraywidth();
 
 	/* draw status first so it can be overdrawn by tags later */
-	if (m == selmon || 1) { /* status is only drawn on selected monitor */
+	if (m == selmon) { /* status is only drawn on selected monitor */
 		drw_setscheme(drw, scheme[SchemeNorm]);
 		sw = TEXTW(stext) - lrpad / 2 + 2; /* 2px right padding */
 		drw_text(drw, m->ww - sw - stw, 0, sw, bh, lrpad / 2 - 2, stext, 0);
