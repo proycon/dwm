0f8570fc93499a5f90f2ac8d98b694c02897375a
Maarten van Gompel <proycon@anaproy.nl>
2021-10-03 21:48:22 +0200

    removed conflicting keybinding, changed some existing ones

--- config.def.h
+++ config.def.h
@@ -98,11 +98,12 @@ static const Layout layouts[] = {
 #define STACKKEYS(MOD,ACTION) \
 	{ MOD, XK_j,     ACTION##stack, {.i = INC(+1) } }, \
 	{ MOD, XK_k,     ACTION##stack, {.i = INC(-1) } }, \
-	{ MOD, XK_grave, ACTION##stack, {.i = PREVSEL } }, \
-	{ MOD, XK_q,     ACTION##stack, {.i = 0 } }, \
+	{ MOD, XK_apostrophe, ACTION##stack, {.i = PREVSEL } }, \
+	{ MOD, XK_g,     ACTION##stack, {.i = 0 } },
+/*
 	{ MOD|ShiftMask, XK_a,     ACTION##stack, {.i = 1 } }, \
 	{ MOD, XK_z,     ACTION##stack, {.i = 2 } }, \
-	{ MOD, XK_x,     ACTION##stack, {.i = -1 } },
+	{ MOD, XK_x,     ACTION##stack, {.i = -1 } },*/
 
 /* helper for spawning shell commands in the pre dwm-5.0 fashion */
 #define SHCMD(cmd) { .v = (const char*[]){ "/bin/sh", "-c", cmd, NULL } }
@@ -135,12 +136,13 @@ static Key keys[] = {
 	{ MODKEY,                       XK_m,      setlayout,      {.v = &layouts[2]} },
 	{ MODKEY,                       XK_z,      focusmonnum,        {.i = 1 } },
 	{ MODKEY,                       XK_x,      focusmonnum,        {.i = 0 } },
+	{ MODKEY,                       XK_q,      focusmon,         {.i = 1 } },
 	{ MODKEY|ShiftMask,             XK_z,      tagmonnum,        {.i = 1 } },
 	{ MODKEY|ShiftMask,             XK_x,      tagmonnum,        {.i = 0 } },
-	{ MODKEY|ControlMask,		XK_comma,  cyclelayout,    {.i = -1 } },
+	{ MODKEY|ControlMask,		    XK_comma,  cyclelayout,    {.i = -1 } },
 	{ MODKEY|ControlMask,           XK_period, cyclelayout,    {.i = +1 } },
-	{ MODKEY,                       XK_apostrophe,  cyclelayout,      {0} },
-	{ MODKEY|ShiftMask,             XK_apostrophe,  togglefloating, {0} },
+	{ MODKEY,                       XK_c,  cyclelayout,      {0} },
+	{ MODKEY|ShiftMask,             XK_c,  togglefloating, {0} },
 	{ MODKEY,                       XK_backslash,  togglefloating, {0} },
 	{ MODKEY,                       XK_s,      togglesticky,   {0} },
 	{ MODKEY,                       XK_comma,  setgaps,        {.i = -3 } },
