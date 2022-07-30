e8ab4c84e63b278eb7b03afd4428ceccfdbb7cea
Maarten van Gompel <proycon@anaproy.nl>
2022-07-30 14:05:41 +0200

    keybinding: local neomutt

--- config.def.h
+++ config.def.h
@@ -172,8 +172,8 @@ static Key keys[] = {
     { ALTKEY,                       XK_F11, spawn, SHCMD("killall fcitx5; setxkbmap proyru && play ~/dotfiles/media/key.wav") },
     { ALTKEY,                       XK_F12, spawn, SHCMD("killall fcitx5; setxkbmap proyar && play ~/dotfiles/media/key.wav") },
     { ALTKEY|ShiftMask,             XK_F12, spawn, SHCMD("killall fcitx5; setxkbmap proylogic && play ~/dotfiles/media/key.wav") },
-	{ MODKEY,						XK_F1, spawn, SHCMD("alacritty --working-directory ~ -t mail -e ssh -Y -A -t anaproy.nl ~/bin/tm_alot") },
-	{ MODKEY,						XK_F2, spawn, SHCMD("alacritty --working-directory ~ -t vim -e ~/bin/tm nvim") },
+	{ MODKEY,						XK_F1, spawn, SHCMD("alacritty --working-directory ~ -t mail -e neomutt") },
+	{ MODKEY,						XK_F2, spawn, SHCMD("alacritty --working-directory ~ -t vim -e nvim") },
 	{ ALTKEY,						XK_F2, spawn, SHCMD("~/bin/lala") }, /* start music */
 	{ ALTKEY|ShiftMask,				XK_F2, spawn, SHCMD("killall snapclient; mpc stop") }, /* stop music */
 	{ MODKEY,						XK_F3, spawn, SHCMD("~/dotfiles/homecommand.sh") },
